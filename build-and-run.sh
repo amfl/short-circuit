a17c1a6632f88425e043d280c8fdf1b957064e6a
NickLarsenNZ <nick@aptiv.co.nz>
2019-10-24 21:22:42 +1300

    Pass script args to run.sh

--- build-and-run.sh
+++ build-and-run.sh
@@ -2,4 +2,4 @@
 # The image only needs to be rebuilt if the dependencies change.
 # The source code is not baked in, it is volume-mounted at runtime.
 
-docker build . -t short-circuit:latest && ./run.sh
+docker build . -t short-circuit:latest && ./run.sh $@
