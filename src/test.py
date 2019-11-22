56e5904d40915754874d2a5d43c0dfdfcf329668
amfl <amfl@none.none>
2019-11-23 12:13:17 +1300

    test: Add test which fails serialization

--- src/test.py
+++ src/test.py
@@ -5,6 +5,27 @@ class TestTest(unittest.TestCase):
     def testThings(self):
         self.assertEqual(True, True)
 
+class BasicTest(unittest.TestCase):
+    """Make sure that the output of the NAND turns on after a few ticks. This
+    should be true regardless of which ticking mechanism is used."""
+    def setUp(self):
+        gridStr = ("-r--\n")
+        self.grid = Grid.deserialize_from_string(gridStr)
+        print('setup done')
+
+    def runTest(self, mechanism):
+        for i in range(5):
+            self.grid.tick(mechanism)
+
+        outputWire = self.grid.get(3,0)
+        self.assertTrue(outputWire.get_output())
+
+    def testMechansimA(self):
+        self.runTest('a')
+
+    def testMechansimB(self):
+        self.runTest('b')
+
 class TickingTest(unittest.TestCase):
     def setUp(self):
         nogap = ("--D\n"
