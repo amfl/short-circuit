b20ca1950f4214b641f17ac3fbec45157c914aa6
amfl <amfl@none.none>
2019-12-07 11:31:54 +1300

    refactor: Move the rewrite stuff to its own dir

--- rewrite-src/test/board_static.py
+++ rewrite-src/test/board_static.py
@@ -1,6 +1,6 @@
 import logging
 import unittest
-from highlevel import Board, Switch, Nand, Wire
+from shortcircuit import Board, Switch, Nand, Wire
 
 class TestTest(unittest.TestCase):
     def testThings(self):
@@ -182,35 +182,6 @@ class DirectSimNodeOutputClockTest(unittest.TestCase):
             self.assertEqual(self.nands[0].output(), self.nands[1].output())
             self.boards[0].tick()
             self.boards[1].tick()
-
-class BridgeTest(unittest.TestCase):
-    """Wires will need to cross each other!"""
-
-    def setUp(self):
-        board_str = (".--l..\n"
-                     "...-..\n"
-                     "-r-|--\n"
-                     "...-..\n")
-        self.board = Board.deserialize(board_str)
-        self.top_nand = self.board.get((3,0))
-        self.right_wire = self.board.get((5,2))
-        self.bridge = self.board.get((3,2))
-
-        self.board.tick()
-        self.board.tick()
-        self.board.tick()
-
-    def testBridgeTransmits(self):
-        """Ensure signal crosses the bridge when it's supposed to"""
-        self.assertTrue(self.right_wire.output())
-
-    def testBridgeDoesNotBleed(self):
-        """Make sure signal isn't "bleeding under" the bridge"""
-        self.assertTrue(self.top_nand.output())
-
-    # def testBridgeHasNoOutput(self):
-    #     """It makes no sense to ask the bridge what its output is... It has two signals."""
-    #     self.assertIsNone(self.bridge.output())
 
 if __name__ == '__main__':
     unittest.main()
---
+++ rewrite-src/test/unimplimented.py
@@ -0,0 +1,35 @@
+import logging
+import unittest
+from shortcircuit import Board, Switch, Nand, Wire
+
+class BridgeTest(unittest.TestCase):
+    """Wires will need to cross each other!"""
+
+    def setUp(self):
+        board_str = (".--l..\n"
+                     "...-..\n"
+                     "-r-|--\n"
+                     "...-..\n")
+        self.board = Board.deserialize(board_str)
+        self.top_nand = self.board.get((3,0))
+        self.right_wire = self.board.get((5,2))
+        self.bridge = self.board.get((3,2))
+
+        self.board.tick()
+        self.board.tick()
+        self.board.tick()
+
+    def testBridgeTransmits(self):
+        """Ensure signal crosses the bridge when it's supposed to"""
+        self.assertTrue(self.right_wire.output())
+
+    def testBridgeDoesNotBleed(self):
+        """Make sure signal isn't "bleeding under" the bridge"""
+        self.assertTrue(self.top_nand.output())
+
+    # def testBridgeHasNoOutput(self):
+    #     """It makes no sense to ask the bridge what its output is... It has two signals."""
+    #     self.assertIsNone(self.bridge.output())
+
+if __name__ == '__main__':
+    unittest.main()
