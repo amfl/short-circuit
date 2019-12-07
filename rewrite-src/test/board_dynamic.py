3f0a2548a1d275daa57b46fe6a40d4bfad5eadf9
amfl <amfl@none.none>
2019-12-07 16:17:37 +1300

    Add tests for breaking a wire group

--- rewrite-src/test/board_dynamic.py
+++ rewrite-src/test/board_dynamic.py
@@ -43,6 +43,44 @@ class TestWireJoin(unittest.TestCase):
         self.assertTrue(final_wire.output())
 
 
+class TestWireBreak(unittest.TestCase):
+    def setUp(self):
+        board_str = ("-R---")
+        self.board = Board.deserialize(board_str)
+
+        self.nand = self.board.get((1, 0))
+
+        # Split the wire
+        self.board.set((3, 0), None)
+
+    def testRemoval(self):
+        """The `set` function should be able to delete wire"""
+        self.assertIsNone(self.board.get((3, 0)))
+
+    def testSplitWiresAreDifferentObjects(self):
+        self.assertIsNot(self.board.get((2, 0)), self.board.get((4, 0)))
+
+    def testIOIsUpdated(self):
+        output_wire = self.board.get((2, 0))
+        self.assertIs(list(output_wire.inputs)[0], self.nand)
+
+        orphaned_wire = self.board.get((4, 0))
+        self.assertEqual(len(orphaned_wire.inputs), 0)
+
+    def testSoftWireSignal(self):
+        """The wire groups should have the right outputs after a tick."""
+        self.board.tick()
+        self.testE2E()
+
+    def testE2E(self):
+        """The wire groups should have the right outputs after the break."""
+        output_wire = self.board.get((2, 0))
+        self.assertTrue(output_wire.output())
+
+        orphaned_wire = self.board.get((4, 0))
+        self.assertFalse(orphaned_wire.output())
+
+
 class TestComponentReplacement(unittest.TestCase):
     """Make sure that joining two wire groups via deleting a component doesn't
     leave incorrect IO information around."""
