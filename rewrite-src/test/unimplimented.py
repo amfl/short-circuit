ef75d84a6c6e541248b9bb383ac290f0b420dd7b
amfl <amfl@none.none>
2019-12-27 00:43:31 +1300

    test: Add tests for unimplemented copy function

--- rewrite-src/test/unimplimented.py
+++ rewrite-src/test/unimplimented.py
@@ -34,5 +34,35 @@ class BridgeTest(unittest.TestCase):
     #     self.assertIsNone(self.bridge.output())
 
 
+@unittest.skip("Board copy operations are unimplemented")
+class CopyTest(unittest.TestCase):
+    """Copying pieces of the board"""
+    def setUp(self):
+        board_str = ("--...-\n"
+                     "d....-\n"
+                     "--...-\n")
+        self.board = Board.deserialize(board_str)
+
+        self.board.copy((0, 0), (1, 2), (3, 0))
+
+    def testCopy(self):
+        copied_str = ("--.---\n"
+                      "d..d.-\n"
+                      "--.---\n")
+        self.assertEqual(self.board.serialize(), copied_str)
+
+    def testIO(self):
+        wire = self.board.get((5, 1))
+        nand = self.board.get((3, 1))
+        self.assertEqual(nand.inputs, {wire})
+        self.assertEqual(wire.inputs, {nand})
+
+    def testE2E(self):
+        nand = self.board.get((3, 1))
+        for i in range(10):
+            self.assertEqual(nand.output(), bool(i))
+            self.board.tick()
+
+
 if __name__ == '__main__':
     unittest.main()
